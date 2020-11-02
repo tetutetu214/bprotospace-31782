85b225224c0030e938e9f9240b5f7577e80ec0dd
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-02 17:49:48 +0900

    ユーザー　アクション・コントロール設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ root to: "prototypes#index"
  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy]do
   resources :comments, only: :create
  end
+ resources :users, only: :show
 end
